6995985f2e0c47b47269587dca3b13c6687c9307
scwebd <scwebd@gmail.com>
2019-01-24 23:34:57 -0700

    Updating starter code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,12 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    // this route should find all contacts in the table and render them using the Handlebars 
-    // 'contacts' template, sorted ascending by firstName
-    app.get("/", function(req, res) {
-
-    });
-    
-    // this route should find all contacts of a particular type (Personal or Business) and render them 
-    // using the Handlebars 'contacts' template, sorted ascending by firstName
-    app.get("/:type", function(req, res) {
-
-        // BONUS: pass the contact type through to the handlebars template... be sure
-        // to make the contact type title-cased!
-
-    });
-
     // this route should find all contacts in the table and display them as JSON
     app.get("/api/contacts", function(req, res) {
     
     });
 
-    // this route should add a new contact to the table, and should then redirect to the route '/api/contacts'
+    // this route should add a new contact to the table
 	app.post("/api/contacts", function(req, res) {
         
     });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,4 +5,19 @@ module.exports = function(app) {
 	app.get("/contacts/new", function(req, res) {
         
     });
+
+    // this route should find all contacts in the table and render them using the Handlebars 
+    // 'contacts' template, sorted ascending by firstName
+    app.get("/", function(req, res) {
+
+    });
+
+    // this route should find all contacts of a particular type (Personal or Business) and render them 
+    // using the Handlebars 'contacts' template, sorted ascending by firstName
+    app.get("/:type", function(req, res) {
+
+        // BONUS: pass the contact type through to the handlebars template... be sure
+        // to make the contact type title-cased!
+
+    });
 }
